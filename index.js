4faa368e31e4cda16acc781c300e9ba23c3bb550
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-14 11:42:34 +0000

    Add restAPI

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+//app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 var apirouter = express.Router();
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 apirouter.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-apirouter.post('/text', function(req, res) {
-  console.log(req.body);
-  console.log(req.body.mytextarea);
+apirouter.route('/textformat').post(function(req, res) {
+  //console.log(req);
+  console.log(req.body.data);
+  res.json({ message: 'hooray! welcome to our api!' }); 
 });
 
 app.use('/api', apirouter);
