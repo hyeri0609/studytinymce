e5285778dbfd32a9d0f082935fe6a0e353bb4961
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-14 11:13:34 +0000

    update text

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+var apirouter = express.Router();
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+apirouter.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+apirouter.post('/text', function(req, res) {
+  console.log(req.body);
+  console.log(req.body.mytextarea);
+});
+
+app.use('/api', apirouter);
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
